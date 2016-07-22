d8f5e70db47ffbadc42eea96b46e6d772723f14c
koryb <koryb@ifit.com>
2016-07-22 14:45:50 -0600

    I0 stuff

--- LearnYouNode/program.js
+++ LearnYouNode/program.js
@@ -1,1 +1,9 @@
-console.log("HELLO WORLD");
+var fs = require('fs');
+
+var buffer = fs.readFileSync(process.argv[2]);
+var str = buffer.toString();
+str = str.split("\n");
+
+
+
+console.log(str.length - 1);
